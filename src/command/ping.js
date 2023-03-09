bc7a583bea7a202c491363ef6524ea4f3583c145
sihyeokpark <exon.indiv@gmail.com>
2023-03-09 22:47:21 +0900

    왜안돼

--- src/command/ping.js
+++ src/command/ping.js
@@ -3,7 +3,11 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+        .addStringOption(option =>
+            option.setName('input')
+                .setDescription('The input to echo back')
+                .setRequired(true)),
 	async execute(interaction) {
 		await interaction.reply('Pong!')
 	},
